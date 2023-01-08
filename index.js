c2a395cfa5eacce06dcc2312d692b1db63412c9d
Sofia E <ermishina.2004@mail.ru>
2023-01-08 18:00:24 +0000

    fredmary

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 const app = express()
-const port = process.env.PORT || 8080 
+const port = process.env.PORT || 8080
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static('public'));
